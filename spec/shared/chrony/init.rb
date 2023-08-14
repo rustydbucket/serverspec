1b9ce173d5bc4e670ebc3f01e5a648f3c5b1f08b
Russell Duma <12658809+rustydbucket@users.noreply.github.com>
2023-08-14 08:49:46 -0400

    remove iburst from servers

--- spec/shared/chrony/init.rb
+++ spec/shared/chrony/init.rb
@@ -5,7 +5,7 @@ shared_examples 'chrony::init' do
         it { should be_installed }
     end
 
-    describe service('chrony') do
+    describe service('chronyd.service') do
         it { should be_enabled }
         it { should be_running }
     end
@@ -15,11 +15,11 @@ shared_examples 'chrony::init' do
         its(:content) { should contain 'logdir /var/log/chrony' }
         its(:content) { should contain 'keyfile /etc/chrony.keys' }
         its(:content) { should contain 'log measurements statistics tracking' }
-        its(:content) { should contain 'server 0.au.pool.ntp.org iburst' }
-        its(:content) { should contain 'server 1.au.pool.ntp.org iburst' }
-        its(:content) { should contain 'server 2.au.pool.ntp.org iburst' }
-        its(:content) { should contain 'server 3.au.pool.ntp.org iburst' }
+        its(:content) { should contain 'server 0.au.pool.ntp.org' }
+        its(:content) { should contain 'server 1.au.pool.ntp.org' }
+        its(:content) { should contain 'server 2.au.pool.ntp.org' }
+        its(:content) { should contain 'server 3.au.pool.ntp.org' }
         its(:content) { should contain 'rtcsync' }
-        its(:content) { should contain 'makesetp 1.0 3' }
+        its(:content) { should contain 'makestep 1.0 3' }
     end
 end
