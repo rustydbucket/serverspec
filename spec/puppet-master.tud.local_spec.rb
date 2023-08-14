93bc383ea830b3e1ae9fae7fc363581638a01067
Russell Duma <12658809+rustydbucket@users.noreply.github.com>
2023-08-14 08:12:28 -0400

    updated based on serverepc_examples from rubyisbeautiful

--- spec/puppet-master.tud.local_spec.rb
+++ spec/puppet-master.tud.local_spec.rb
@@ -2,5 +2,4 @@ require 'spec_helper'
 
 describe 'puppet-master.tud.local' do
   include_examples 'chrony::init'
-  include_examples 'auditd::init'
 end
--- spec/redhat8/system_audit_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'spec_helper'
-
-set :os, :family => 'redhat'
-
